7ed33eebc9faca57bec0f1535ebfbad6a371972d
zhmlst <zhmlst.btw@gmail.com>
2025-09-22 01:43:25 +0300

    change(slices): remove useless functions

--- slice/slice.go
+++ slice/slice.go
@@ -10,35 +10,6 @@ func Filter[T any](items []T, filter func(T) bool) []T {
 	return filtered
 }
 
-func AndFilters[T any](items []T, filters ...func(it T) bool) []T {
-	filtered := make([]T, 0, len(items))
-	for _, item := range items {
-		ok := true
-		for _, filter := range filters {
-			if !filter(item) {
-				ok = false
-				break
-			}
-		}
-		if ok {
-			filtered = append(filtered, item)
-		}
-	}
-	return filtered
-}
-
-func OrFilters[T any](items []T, filters ...func(it T) bool) []T {
-	filtered := make([]T, 0, len(items))
-	for _, item := range items {
-		for _, filter := range filters {
-			if filter(item) {
-				filtered = append(filtered, item)
-			}
-		}
-	}
-	return filtered
-}
-
 func Map[T, R any](items []T, mapper func(it T) R) []R {
 	mapped := make([]R, len(items))
 	for i, item := range items {
